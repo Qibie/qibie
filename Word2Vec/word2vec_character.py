553813422bd758f0cfb04300b24e863b1ce6d3b4
herewithyou <1274108397@qq.com>
2018-03-17 19:58:25 +0800

    fuck

---
+++ Word2Vec/word2vec_character.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: curry
+@software: Pycharm
+@file: word2vec.py
+@time: 2018/3/17
+@description: 训练字向量
+"""
+
+import gensim,logging
+from Word2Vec import Iterator
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+sentences=Iterator.Iterator(r'/home/curry/NER/patent/character_segmentation/allwords.txt')
+model=gensim.models.Word2Vec(sentences=sentences,sg=1,window=8,min_count=1,size=100,iter=10,hs=1,negative=10)
+model.save('./character_model/model')
